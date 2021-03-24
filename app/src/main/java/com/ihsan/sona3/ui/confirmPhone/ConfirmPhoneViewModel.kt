b6d2fceb9c910c24cf303caff2aed5ab4cb927cd
islam.ar <islam.ahmed@approcks.com>
2021-03-24 22:12:09 +0200

    Add login Screens

---
+++ app/src/main/java/com/ihsan/sona3/ui/confirmPhone/ConfirmPhoneViewModel.kt
@@ -0,0 +1,7 @@
+package com.ihsan.sona3.ui.confirmPhone
+
+import androidx.lifecycle.ViewModel
+
+class ConfirmPhoneViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/ihsan/sona3/ui/login/LoginViewModel.kt
@@ -0,0 +1,7 @@
+package com.ihsan.sona3.ui.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
